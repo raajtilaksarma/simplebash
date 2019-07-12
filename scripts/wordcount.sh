8e4053138fb8143069bc238e9c60dcc72abdfa9e
raaj <raajtilaksarma@gmail.com>
2019-07-12 11:10:46 +0530

    count no of times each word occurs in a file

---
+++ scripts/wordcount.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+programname=$0
+function usage(){ 
+    echo "usage: $programname [filename]"
+    echo "  -h      display help"
+    exit 1
+}
+
+if [[ ( $@ == "--help") ||  $@ == "-h" ]]; then 
+	usage
+	exit 0
+fi
+
+if [ x"$1" = "x" ]; then
+        echo "Filename required!"
+        exit 1
+fi
+awk '{for(x=1; $x; ++x)print $x}' "${1}" | sort | uniq -c
